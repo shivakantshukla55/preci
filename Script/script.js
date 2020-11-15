da9e81f95ea8f2751744ddeb84296d8c9af721ae
shivakant shukla <shivakantshukla55@gmail.com>
2020-11-16 02:27:12 +0530

    form added

--- Script/script.js
+++ Script/script.js
@@ -25,3 +25,16 @@ window.onscroll = function () {
 //   cellAlign: 'left',
 //   contain: true
 // });
+
+
+// form starts 
+const checkbox = document.querySelector('.my-form input[type="checkbox"]');
+const btns = document.querySelectorAll(".my-form button");
+
+checkbox.addEventListener("change", function() {
+  const checked = this.checked;
+  for (const btn of btns) {
+    checked ? (btn.disabled = false) : (btn.disabled = true);
+  }
+});
+// form ends 
