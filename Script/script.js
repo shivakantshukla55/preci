26361c51f06f4538fd7ee4d5d27535b10a600812
shivakantshukla55 <shivakantshukla55@gmail.com>
2021-01-16 01:18:47 +0530

    products animation added

--- Script/script.js
+++ Script/script.js
@@ -208,3 +208,17 @@ myForm.addEventListener('submit', function(){
 
 //git test
 // test git for navbar-fix
+
+const boxes = document.querySelectorAll(".row");
+
+const checkBoxes = () => {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerBottom) box.classList.add("show");
+    else box.classList.remove("show");
+  });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes();
